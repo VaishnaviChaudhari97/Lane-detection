b31f98b770f07673f65987313478e472df115ecc
vaishnavi <vaishnavi.rc97@gmail.com>
2021-05-24 08:15:48 -0500

    optimizing

--- lane-detection.py
+++ lane-detection.py
@@ -1,6 +1,36 @@
 import numpy as np
 import cv2
 
+def make_points(image, line):
+    slope, intercept = line
+    y1 = int(image.shape[0])# bottom of the image
+    y2 = int(y1*3/5)         # slightly lower than the middle
+    x1 = int((y1 - intercept)/slope)
+    x2 = int((y2 - intercept)/slope)
+    return [[x1, y1, x2, y2]]
+ 
+def average_slope_intercept(image, lines):
+    left_fit    = []
+    right_fit   = []
+    if lines is None:
+        return None
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            fit = np.polyfit((x1,x2), (y1,y2), 1)
+            slope = fit[0]
+            intercept = fit[1]
+            if slope < 0: # y is reversed in image
+                left_fit.append((slope, intercept))
+            else:
+                right_fit.append((slope, intercept))
+    # add more weight to longer lines
+    left_fit_average  = np.average(left_fit, axis=0)
+    right_fit_average = np.average(right_fit, axis=0)
+    left_line  = make_points(image, left_fit_average)
+    right_line = make_points(image, right_fit_average)
+    averaged_lines = [left_line, right_line]
+    return averaged_lines
+
 def canny(img):
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     kernel = 5
@@ -28,13 +58,11 @@ def display_lines(img,lines):
 
 image = cv2.imread('C:/Users/vaish/OneDrive/Documents/AI Project/test_image.jpg')
 lane_image = np.copy(image)
-#gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-#blur = cv2.GaussianBlur(gray,(5,5),0)
-#canny = cv2.Canny(gray, 50, 150)
 canny = canny(lane_image)
 cropped_image = region_of_interest(canny)
 lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
-lines_image = display_lines(lane_image, lines)
+averaged_lines = average_slope_intercept(lane_image, lines)
+lines_image = display_lines(lane_image, averaged_lines)
 combo_image = cv2.addWeighted(lane_image, 0.8, lines_image, 1,1)
 cv2.imshow("result", combo_image)
 cv2.waitKey(0)
