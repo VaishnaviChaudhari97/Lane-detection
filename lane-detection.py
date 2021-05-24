93cb5471fc5d7c208dfb5b92ac902793a4186470
vaishnavi <vaishnavi.rc97@gmail.com>
2021-05-24 08:18:16 -0500

    line detection on video

--- lane-detection.py
+++ lane-detection.py
@@ -1,6 +1,6 @@
-import numpy as np
 import cv2
-
+import numpy as np
+ 
 def make_points(image, line):
     slope, intercept = line
     y1 = int(image.shape[0])# bottom of the image
@@ -30,24 +30,14 @@ def average_slope_intercept(image, lines):
     right_line = make_points(image, right_fit_average)
     averaged_lines = [left_line, right_line]
     return averaged_lines
-
+ 
 def canny(img):
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     kernel = 5
     blur = cv2.GaussianBlur(gray,(kernel, kernel),0)
     canny = cv2.Canny(gray, 50, 150)
     return canny
-
-def region_of_interest(canny):
-    height = canny.shape[0]
-    polygons = np.array([
-        [(200, height), (1100, height), (550, 250)]
-    ])
-    mask = np.zeros_like(canny)
-    cv2.fillPoly(mask, polygons, 255)
-    masked_image = cv2.bitwise_and(canny, mask)
-    return masked_image
-
+ 
 def display_lines(img,lines):
     line_image = np.zeros_like(img)
     if lines is not None:
@@ -55,14 +45,42 @@ def display_lines(img,lines):
             for x1, y1, x2, y2 in line:
                 cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),10)
     return line_image
+ 
+def region_of_interest(canny):
+    height = canny.shape[0]
+    width = canny.shape[1]
+    mask = np.zeros_like(canny)
+ 
+    triangle = np.array([[
+    (200, height),
+    (550, 250),
+    (1100, height),]], np.int32)
+ 
+    cv2.fillPoly(mask, triangle, 255)
+    masked_image = cv2.bitwise_and(canny, mask)
+    return masked_image
+ 
+# image = cv2.imread('C:/Users/vaish/OneDrive/Documents/AI Project/test_image.jpg')
+# lane_image = np.copy(image)
+# lane_canny = canny(lane_image)
+# cropped_canny = region_of_interest(lane_canny)
+# lines = cv2.HoughLinesP(cropped_canny, 2, np.pi/180, 100, np.array([]), minLineLength=40,maxLineGap=5)
+# averaged_lines = average_slope_intercept(image, lines)
+# line_image = display_lines(lane_image, averaged_lines)
+# combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 0)
+ 
 
-image = cv2.imread('C:/Users/vaish/OneDrive/Documents/AI Project/test_image.jpg')
-lane_image = np.copy(image)
-canny = canny(lane_image)
-cropped_image = region_of_interest(canny)
-lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
-averaged_lines = average_slope_intercept(lane_image, lines)
-lines_image = display_lines(lane_image, averaged_lines)
-combo_image = cv2.addWeighted(lane_image, 0.8, lines_image, 1,1)
-cv2.imshow("result", combo_image)
-cv2.waitKey(0)
+cap = cv2.VideoCapture("C:/Users/vaish/OneDrive/Documents/AI Project/test2.mp4")
+while(cap.isOpened()):
+    _, frame = cap.read()
+    canny_image = canny(frame)
+    cropped_canny = region_of_interest(canny_image)
+    lines = cv2.HoughLinesP(cropped_canny, 2, np.pi/180, 100, np.array([]), minLineLength=40,maxLineGap=5)
+    averaged_lines = average_slope_intercept(frame, lines)
+    line_image = display_lines(frame, averaged_lines)
+    combo_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
+    cv2.imshow("result", combo_image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
