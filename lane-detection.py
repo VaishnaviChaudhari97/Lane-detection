7496b05d368a94f7aad593b00a23aec7c6ca2f48
vaishnavi <vaishnavi.rc97@gmail.com>
2021-05-24 07:36:54 -0500

    smoothening

--- lane-detection.py
+++ lane-detection.py
@@ -4,5 +4,6 @@ import cv2
 image = cv2.imread('C:/Users/vaish/OneDrive/Documents/AI Project/test_image.jpg')
 lane_image = np.copy(image)
 gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-cv2.imshow("result", gray)
+blur = cv2.GaussianBlur(gray,(5,5),0)
+cv2.imshow("result", blur)
 cv2.waitKey(0)
