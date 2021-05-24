c2009a29605dbe63893ce8b64f9a34a66cc30fb7
vaishnavi <vaishnavi.rc97@gmail.com>
2021-05-24 07:38:31 -0500

    edge detection

--- lane-detection.py
+++ lane-detection.py
@@ -5,5 +5,6 @@ image = cv2.imread('C:/Users/vaish/OneDrive/Documents/AI Project/test_image.jpg'
 lane_image = np.copy(image)
 gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 blur = cv2.GaussianBlur(gray,(5,5),0)
-cv2.imshow("result", blur)
+canny = cv2.Canny(gray, 50, 150)
+cv2.imshow("result", canny)
 cv2.waitKey(0)
